95782f6fcddfeeafb2ef87894de015820729b2e9
stijnrosaer <stijn.rosaer@student.uantwerpen.be>
2020-08-26 13:55:19 +0200

    Convert block to class

---
+++ block.py
@@ -0,0 +1,19 @@
+class Block:
+    def __init__(self, index, timestamp, transactions, proof, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.proof = proof
+        self.previous_hash = previous_hash
+
+    def __dict__(self):
+        return {
+            'index': self.index,
+            'timestamp': self.timestamp,
+            'transactions': self.transactions,
+            'proof': self.proof,
+            'previous_hash': self.previous_hash
+        }
+
+    def __str__(self):
+        return self.__dict__()
